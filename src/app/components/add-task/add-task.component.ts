38ab505afeba132ea74a5102b3d3f68ef3bf119b
Andrew Albrecht <andrewalbrecht@utexas.edu>
2021-05-19 17:17:03 -0500

    Add task functionality, forms module, submit and create task, toggle button and task form with ui service

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { UiService } from '../../services/ui.service'
+import {Subscription} from 'rxjs'
 import {Task} from '../../Task'
-import { of } from 'rxjs';
 
 @Component({
   selector: 'app-add-task',
@@ -12,8 +13,13 @@ export class AddTaskComponent implements OnInit {
   text: string;
   day: string;
   reminder: boolean = false;
+  showAddTask: boolean;
+  subscription: Subscription;
 
-  constructor() { }
+  constructor(private uiService:UiService) { 
+    this.subscription = this.uiService.onToggle().subscribe((value) => (this.showAddTask = value));
+  }
+
 
   ngOnInit(): void {
   }
